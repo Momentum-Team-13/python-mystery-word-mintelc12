26baeacbd7419cb1f139e2ec9f867a8182194ee5
Chloe Mintel <mintelc12@gmail.com>
2022-06-22 11:41:17 -0400

    able to append letters correctly and incorrectly guessed.

--- mystery_word.py
+++ mystery_word.py
@@ -5,16 +5,10 @@ def play_game():
     print(f"answer: {answer}")
     print("_" * len(answer))
 
-    guesses = []
-
-    while len(guesses) < 8:
-        guesses.append(guess_character())
-    print(f"Your guesses: {guesses}")
+    user_guesses_letter(answer)
 
     # check guesses list against answer
     # render new result
-    new_list = [char for char in guesses if char in answer]
-    print(new_list)
 
 
 def generate_word_for_game():
@@ -37,25 +31,28 @@ def generate_word_for_game():
     list(answer_string)
     print(list(answer_string))
 
+    return answer_string
+
+def user_guesses_letter(answer_string):
     # Appends correct letters to a list for user to see
+
     correct_letter_guessed = []
     incorrect_letter_guessed = []
-    letter = input("")
-    # for letter in list(answer_string):
-    if any([letter in list(answer_string)]):
+    while len(incorrect_letter_guessed) < 8:
+        letter = guess_character()
+
+        # for letter in list(answer_string):
+        if any([letter in list(answer_string)]):
             correct_letter_guessed.append(letter)
             print(f"Your guess of {letter} is in the mystery word!")
             print(f"Correct letters:{correct_letter_guessed}")
-    else: 
+        else: 
             print(f"Sorry, {letter} is not in the mystery word.")
             
-    # Appends incorrect letters to a list for user to see    
+            # Appends incorrect letters to a list for user to see    
             incorrect_letter_guessed.append(letter)
-    print(f"Incorrect letters:{incorrect_letter_guessed}")
+            print(f"Incorrect letters:{incorrect_letter_guessed}")
     
-    return answer
-
-
 def guess_character():
     guess = input("Guess a letter:\n")
 
