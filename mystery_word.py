4eea5eb169f1dca5c0f2f491fb1d7ce9c7475bf0
Chloe Mintel <mintelc12@gmail.com>
2022-06-22 10:49:24 -0400

    working through some for loop issues

--- mystery_word.py
+++ mystery_word.py
@@ -7,7 +7,7 @@ def play_game():
 
     guesses = []
 
-    while len(guesses) <= 8:
+    while len(guesses) < 8:
         guesses.append(guess_character())
     print(f"Your guesses: {guesses}")
 
@@ -29,11 +29,41 @@ def generate_word_for_game():
     # Selet and return a random word from list
     index = random.randint(0, (len(word_list) - 1))
     answer = word_list[index]
+
+    # converts random word into string
+    answer_string = str(answer)
+
+    # displays letters of random word in list and prints list
+    list(answer_string)
+    print(list(answer_string))
+
+    # Appends correct letters to a list for user to see
+    correct_letter_guessed = []
+    letter = input("")
+    for letter in list(answer_string):
+        if any([letter in list(answer_string)]):
+            correct_letter_guessed.append(letter)
+            print(f"Your guess of {letter} is in the mystery word!")
+        else:
+            print(f"Sorry, {letter} is not in the mystery word.")
+    print(f"Correct letters:{correct_letter_guessed}")
+
+    # Appends incorrect letters to a list for user to see    
+    incorrect_letter_guessed = []
+    # for letter in answer_string:
+    if letter not in answer_string:
+            incorrect_letter_guessed.append(letter)
+    print(f"Incorrect letters:{incorrect_letter_guessed}")
+    
     return answer
+
 
 def guess_character():
     guess = input("Guess a letter:\n")
+
     return guess
+
+
 
 if __name__ == "__main__":
     play_game()
